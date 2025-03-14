93787cb76cbe53615ad8928be847b1c2ef6f900c
jstrieb/github-stats <wtb.developer1@gmail.com>
2025-03-15 00:57:05 +0200

    Update SingleMessage.tsx

--- src/pages/DialogsPage/MessagesList/SingleMessage/SingleMessage.tsx
+++ src/pages/DialogsPage/MessagesList/SingleMessage/SingleMessage.tsx
@@ -1,45 +1,39 @@
-import React, { useRef } from "react";
-import styles from "./SingleMessage.module.css";
-import { formatDate } from "../../../../utils/formatDate";
-import readIcon from "../../../../components/Other/read.png";
-import defaultUser from "../../../../components/Other/user.png";
-import { useSelector } from "react-redux";
-import { selectMe } from "../../../../redux/auth/selectors";
-import Button from "@mui/material/Button";
-import { MdDeleteForever } from "react-icons/md";
-import { selectIdActiveDialog } from "../../../../redux/dialogs/selectors";
+import React, { useRef } from 'react';
+import styles from './SingleMessage.module.css';
+import { formatDate } from '../../../../utils/formatDate';
+import readIcon from '../../../../components/Other/read.png';
+import defaultUser from '../../../../components/Other/user.png';
+import { useSelector } from 'react-redux';
+import { selectMe } from '../../../../redux/auth/selectors';
+import Button from '@mui/material/Button';
+import { MdDeleteForever } from 'react-icons/md';
+import { selectIdActiveDialog } from '../../../../redux/dialogs/selectors';
 
-export const SingleMessage = ({ message }) => {
+export const SingleMessage = ({ message }: any) => {
   const currentUser = useSelector(selectMe);
   const idActiveDialog = useSelector(selectIdActiveDialog);
   const wsService = useRef(null);
 
   const handleDeleteMessage = () => {
     if (wsService.current) {
-      // @ts-ignore
-      const message = {
-        type: "delete_message",
+      const deleteMessagePayload = {
+        type: 'delete_message',
         dialog_id: idActiveDialog,
-        message_id: message?.id,
+        message_id: message.id,
       };
       // @ts-ignore
-      wsService.current.send(JSON.stringify(message));
+      wsService.current.send(JSON.stringify(deleteMessagePayload));
     }
   };
 
   return (
     <div
-      {/* eslint-disable-next-line react/prop-types */}
-      className={`${styles.message} ${message.sender?.id === currentUser?.id ? styles.myMessage : ""}`}
+      className={`${styles.message} ${message.sender?.id === currentUser?.id ? styles.myMessage : ''}`}
       key={message.id}
     >
       <div className={styles.messageUserInfo}>
         <img
-          src={
-            message.sender?.profile_picture
-              ? message.sender?.profile_picture
-              : defaultUser
-          }
+          src={message.sender?.profile_picture ? message.sender?.profile_picture : defaultUser}
           alt="userPhoto"
         />
       </div>
@@ -53,11 +47,7 @@ export const SingleMessage = ({ message }) => {
         <span>{formatDate(message?.updated_at)}</span>
         {message.sender?.id === currentUser?.id && (
           <Button onClick={handleDeleteMessage}>
-            <MdDeleteForever
-              className={styles.deleteDialogIcon}
-              color="#0f4fe2"
-              size={25}
-            />
+            <MdDeleteForever className={styles.deleteDialogIcon} color="#0f4fe2" size={25} />
           </Button>
         )}
       </div>
